adc91220d8d7ae5792f92d5b2699ab1771a8d0d4
Dan Cottam <dan.cottam@landregistry.gov.uk>
2023-12-01 10:14:56 +0000

    Initial setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,20 @@ plugins {
     kotlin("jvm") version "1.9.20"
 }
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val junitVersion = "5.10.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
 sourceSets {
-    main {
+    /*main {
         kotlin.srcDir("src")
-    }
+    }*/
 }
 
 tasks {
